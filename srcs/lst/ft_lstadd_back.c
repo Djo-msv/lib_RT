e5f1bda63e85261eb3cb9660131881d6e6dc843a
nathan <nathan.rolland066@gmail.com>
2025-05-28 18:53:29 +0200

    modif addback

--- srcs/lst/ft_lstadd_back.c
+++ srcs/lst/ft_lstadd_back.c
@@ -6,24 +6,25 @@
 /*   By: nrolland <nrolland@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 17:46:21 by nrolland          #+#    #+#             */
-/*   Updated: 2025/05/27 15:55:55 by nrolland         ###   ########.fr       */
+/*   Updated: 2025/05/28 18:51:23 by nrolland         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lib_rt.h"
 
-void	ft_lstadd_back(t_list **lst, t_list *new)
+int	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*current;
 
 	if (!lst || !new)
-		return ;
+		return (1);
 	if (!*lst)
 	{
 		*lst = new;
-		return ;
+		return (0);
 	}
 	current = *lst;
 	current = ft_lstlast(current);
 	current->next = new;
+	return (0);
 }
